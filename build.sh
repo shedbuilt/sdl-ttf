6f1c04c05ea07dce3ec3b583a81e844ba47d6332
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:17:08 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./configure --prefix=/usr \
             --build=$SHED_NATIVE_TARGET \
             --disable-static && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
